6e57b4065a06bd11bc13ba69f2fd39e72d819617
hcastagnet <55395507+hcastagnet@users.noreply.github.com>
2019-12-16 17:40:21 +0100

    exo1

---
+++ exo1/index.php
@@ -0,0 +1,31 @@
+<?php require("User.php"); ?>
+
+<html>
+    <head>
+    </head>
+    <body>
+        <?php echo"Exo1";
+        
+            $Utilisateur1 = new User(" Henri","1234");
+            //$Utilisateur1->setNom(" Henri");
+            $Utilisateur1->afficherNom();
+
+           ?>
+
+           
+                <form method="POST" action="index.php">
+            <label> Utilisateur </label>
+                <input type="text" name="champ1">
+            <label> Mot de passe </label>
+                <input type="text" name="champ2">
+                <input type="submit" value="Valider"/>
+        </form>
+        <?php
+        if (isset ($_POST['champ1'])){
+            $isConnect = $Utilisateur1->verifmdp($_POST['nom'],$_POST['MDP']);
+           if ($isConnect){echo "connecté";} else{echo "erreur";}
+         }
+
+        ?>
+    </body>
+</html>
