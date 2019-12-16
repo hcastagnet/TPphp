8757616aa661bd0709914a025912530d76282b5e
hcastagnet <55395507+hcastagnet@users.noreply.github.com>
2019-12-16 17:27:49 +0100

    Exo1

--- exo1/User.php
+++ exo1/User.php
@@ -4,6 +4,10 @@ class User{
     private $_nom;
     private $_MDP = '123';
 
+    public function __construct($nom,$MDP){
+        $this->_nom = $nom;
+        $this->_MDP = $MDP;
+    }
 
     //méthode :
     public function afficherNom(){
@@ -23,3 +27,35 @@ class User{
 }
 
 ?>
+
+<pre>
+<?php
+class User{
+    //propriété :
+    private $_nom;
+    private $_MDP = '123';
+
+    public function __construct($nom,$MDP){
+        $this->_nom = $nom;
+        $this->_MDP = $MDP;
+    }
+
+    //méthode :
+    public function afficherNom(){
+            echo"<p> Le prenom est".$this->_nom."</p>";
+    }
+    public function setNom($NouveauNom){
+        $this->_nom = $NouveauNom;
+    }
+    public function verifmdp($LeNom,$LeMdp){
+        if($LeNom == $this->_nom){
+            if($LeMdp == $this->_MDP){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+?>
+</pre>
